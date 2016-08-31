1f3358b1e8f8cf8568e407df4cd204915fd3dfa7
Rurik19 <rurik19@yandex.ru>
2016-08-31 16:20:54 +0500

    favicon + gitignore

--- js/consts.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.oauthPath = "/oauth/v2/token";
-exports.entriesPath = "/api/entries";
-exports.apiVersionPath = "/api/version";
-exports.tagsPath = "/api/tags";
-exports.formatEnd = ".json";
-//# sourceMappingURL=consts.js.map
