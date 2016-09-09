8fefd2bdf0145bd06f40e8cbf0fa26cfcd462dad
Rurik19 <rurik19@yandex.ru>
2016-09-09 16:07:56 +0500

    renews typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,7 +2,6 @@
 /// <reference path="globals/classnames/index.d.ts" />
 /// <reference path="globals/filesystem/index.d.ts" />
 /// <reference path="globals/filewriter/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react-redux/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
